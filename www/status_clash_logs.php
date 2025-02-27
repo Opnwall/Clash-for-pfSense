9b561495aa92d2b1e6f6cda9989ec014a064cf9c
opnwall <52145756+opnwall@users.noreply.github.com>
2025-02-27 15:14:11 +0800

    Delete www directory

--- www/status_clash_logs.php
+++
@@ -1,4 +0,0 @@
-<?php
-header('Content-Type: text/plain');
-echo shell_exec("tail -n 100 /var/log/clash.log 2>&1");
-?>
--- www/status_sing_box_logs.php
+++
@@ -1,4 +0,0 @@
-<?php
-header('Content-Type: text/plain');
-echo shell_exec("tail -n 100 /var/log/sing-box.log 2>&1");
-?>
